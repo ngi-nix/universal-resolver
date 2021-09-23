9035d62260219ba5ac6e27d1506e932a7c9d261e
Akshat Agarwal <humancalico@disroot.org>
2021-09-23 19:07:55 +0530

    use the v0.4.0 tag of universal-resolver
    non released versions (untagged) of universal-resolver use SNAPSHOT
    dependencies which don't guarantee reproducibility. From the looks of it
    tagged releases don't contain any SNAPSHOT dependencies so this should
    work.

--- flake.nix
+++ flake.nix
@@ -1,90 +1,90 @@
 {
-  description = ""; # TODO
+  description = "Universal Resolver implementation and drivers";
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
     universal-resolver-src = {
-      url = "github:decentralized-identity/universal-resolver/5c2147f7992dd2c622aaf659309131e4cb881c05";
+      url = "github:decentralized-identity/universal-resolver/v0.4.0";
       flake = false;
     };
   };
 
   outputs = { self, nixpkgs, utils, universal-resolver-src }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        src = universal-resolver-src;
-        # TODO remove this once [this](https://github.com/NixOS/nixpkgs/pull/132287#pullrequestreview-736233478) gets merged
-        modifiedJetty = pkgs.jetty.overrideAttrs (oldAttrs: rec {
-          installPhase = ''
-            mkdir -p $out
-            mv bin etc lib modules start.ini start.jar $out
-          '';
-        });
-        repository = pkgs.stdenv.mkDerivation {
-          name = "maven-repository";
-          buildInputs = with pkgs; [ maven ];
+    utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages."${system}";
 
-          src = universal-resolver-src;
+          # TODO remove this once [this](https://github.com/NixOS/nixpkgs/pull/132287#pullrequestreview-736233478) gets merged
+          modifiedJetty = pkgs.jetty.overrideAttrs (
+            oldAttrs: rec {
+              installPhase = ''
+                mkdir -p $out
+                mv bin etc lib modules start.ini start.jar $out
+              '';
+            }
+          );
+          repository = pkgs.stdenv.mkDerivation {
+            name = "maven-repository";
+            buildInputs = with pkgs; [ maven ];
 
-          buildPhase = ''
-            mvn package -Dmaven.repo.local=$out
-          '';
+            src = universal-resolver-src;
 
-          # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files with lastModified timestamps inside
-          installPhase = ''
-            find $out -type f \
-              -name \*.lastUpdated -or \
-              -name resolver-status.properties -or \
-              -name _remote.repositories \
-              -delete
-          '';
+            buildPhase = ''
+              mvn package -Dmaven.repo.local=$out
+            '';
 
-          # don't do any fixup
-          dontFixup = true;
-          outputHashAlgo = "sha256";
-          outputHashMode = "recursive";
-          # replace this with the correct SHA256
-          outputHash = "sha256-M3VDlwQM6cnkJKy3i22YTzs1cPKJ0Dr4ypJpimQ+tmM=";
-        };
-      in
-      rec {
-        # `nix build`
-        packages.uni-resolver-web = pkgs.stdenv.mkDerivation rec {
-          pname = "uni-resolver-web";
-          name = "uni-resolver-web";
-          version = "0.3-SNAPSHOT";
+            # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files with lastModified timestamps inside
+            installPhase = ''
+              find $out -type f \
+                -name \*.lastUpdated -or \
+                -name resolver-status.properties -or \
+                -name _remote.repositories \
+                -delete
+            '';
 
-          src = universal-resolver-src;
+            # don't do any fixup
+            dontFixup = true;
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            # replace this with the correct SHA256
+            outputHash = "sha256-1HmNyuymu6uIu8/d5zobjZ08a0fMC8ZapAT2/Upph/I=";
+          };
+        in
+          rec {
+            # `nix build`
+            packages.uni-resolver-web = pkgs.stdenv.mkDerivation rec {
+              pname = "uni-resolver-web";
+              name = "uni-resolver-web";
+              version = "0.4.0";
 
-          buildInputs = with pkgs; [ maven ];
+              src = universal-resolver-src;
 
-          buildPhase = ''
-            echo "Using repository ${repository}"
-            mvn --offline -Dmaven.repo.local=${repository} package
-          '';
+              buildInputs = with pkgs; [ maven ];
 
-          installPhase = ''
-            mkdir -p $out/webapps
-            cp config.json $out/config.json
-            cp uni-resolver-web/target/${pname}-${version}.war $out/webapps/ROOT.war
-            cd $out
-            ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --create-startd
-            ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --add-to-start=http,deploy,websocket,ext,jsp,jstl,resources,server
-          '';
-        };
-        defaultPackage = packages.uni-resolver-web;
+              buildPhase = ''
+                echo "Using repository ${repository}"
+                mvn --offline -Dmaven.repo.local=${repository} package
+              '';
 
-        # FIXME `nix run`
-        apps.uni-resolver-web = utils.lib.mkApp {
-          drv = packages.uni-resolver-web;
-        };
-        defaultApp = apps.uni-resolver-web;
+              installPhase = ''
+                mkdir -p $out/webapps
+                cp config.json $out/config.json
+                cp uni-resolver-web/target/${pname}-${version}.war $out/webapps/ROOT.war
+                cd $out
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --create-startd
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --add-to-start=http,deploy,websocket,ext,jsp,jstl,resources,server
+              '';
+            };
+            defaultPackage = packages.uni-resolver-web;
 
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ adoptopenjdk-bin maven modifiedJetty ];
-        };
-      });
+            # TODO  `nix run`
+
+            # `nix develop`
+            devShell = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [ adoptopenjdk-bin maven modifiedJetty ];
+            };
+          }
+    );
 }
