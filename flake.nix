4a579327fe25ce9b3ea617c85eb84d6bfa6ae151
Akshat Agarwal <humancalico@disroot.org>
2021-09-27 21:42:51 +0530

    add btcr, sov drivers

--- flake.nix
+++ flake.nix
@@ -8,9 +8,17 @@
       url = "github:decentralized-identity/universal-resolver/v0.4.0";
       flake = false;
     };
+    driver-sov-src = {
+      url = "github:decentralized-identity/uni-resolver-driver-did-sov/0.2.0";
+      flake = false;
+    };
+    driver-btcr-src = {
+      url = "github:decentralized-identity/uni-resolver-driver-did-btcr/0.1.0";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils, universal-resolver-src }:
+  outputs = { self, nixpkgs, utils, universal-resolver-src, driver-sov-src, driver-btcr-src }:
     utils.lib.eachDefaultSystem (
       system:
         let
@@ -53,12 +61,31 @@
               outputHash = "sha256-lnjizQvaz4gRxqQX76ERDxhEN1QH7D5iUEorljDU/ho=";
             }
           );
+          driver-sov-repository = generic-repository.overrideAttrs (
+            oldAttrs: {
+              name = "driver-sov-repository";
+              src = driver-sov-src;
+              buildPhase = ''
+                mvn package -P war -Dmaven.repo.local=$out
+              '';
+              outputHash = "sha256-Ah5G/eaiXcOfAklodrtf0F5S8+2QaQgtVWPKFlF4Dcg=";
+            }
+          );
+          driver-btcr-repository = generic-repository.overrideAttrs (
+            oldAttrs: {
+              name = "driver-btcr-repository";
+              src = driver-btcr-src;
+              buildPhase = ''
+                mvn package -Dmaven.repo.local=$out
+              '';
+              outputHash = pkgs.lib.fakeHash; #"";
+            }
+          );
         in
           rec {
             # `nix build`
             packages.uni-resolver-web = pkgs.stdenv.mkDerivation rec {
               pname = "uni-resolver-web";
-              name = "uni-resolver-web";
               version = "0.4.0";
 
               src = universal-resolver-src;
@@ -66,14 +93,60 @@
               buildInputs = with pkgs; [ maven ];
 
               buildPhase = ''
-                echo "Using repository ${universal-resolver-repository}"
+                echo "using repository ${universal-resolver-repository}"
                 mvn --offline -Dmaven.repo.local=${universal-resolver-repository} package
               '';
 
               installPhase = ''
                 mkdir -p $out/webapps
                 cp config.json $out/config.json
-                cp uni-resolver-web/target/${pname}-${version}.war $out/webapps/ROOT.war
+                cp uni-resolver-web/target/*.war $out/webapps/ROOT.war
+                cd $out
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --create-startd
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --add-to-start=http,deploy,websocket,ext,jsp,jstl,resources,server
+              '';
+            };
+            packages.driver-sov = pkgs.stdenv.mkDerivation rec {
+              pname = "driver-sov";
+              version = "0.2.0";
+
+              src = driver-sov-src;
+
+              nativeBuildInputs = with pkgs; [ maven ];
+
+              buildPhase = ''
+                echo "using repository ${driver-sov-repository}"
+                mvn --offline -Dmaven.repo.local=${driver-sov-repository} package -P war
+              '';
+
+              installPhase = ''
+                mkdir -p $out/sovrin
+                cp -r sovrin $out/sovrin
+                mkdir -p $out/webapps
+                ls target/
+                cp target/*.war $out/webapps/ROOT.war
+                cd $out
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --create-startd
+                ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --add-to-start=http,deploy,websocket,ext,jsp,jstl,resources,server
+              '';
+            };
+            packages.driver-btcr = pkgs.stdenv.mkDerivation rec {
+              pname = "driver-btcr";
+              version = "0.1.0";
+
+              src = driver-btcr-src;
+
+              nativeBuildInputs = with pkgs; [ maven ];
+
+              buildPhase = ''
+                echo "using repository ${driver-btcr-repository}"
+                mvn --offline -Dmaven.repo.local=${driver-btcr-repository} package -P war
+              '';
+
+              installPhase = ''
+                mkdir -p $out/webapps
+                ls target/
+                cp target/*.war -t $out/webapps/ROOT.war
                 cd $out
                 ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --create-startd
                 ${pkgs.adoptopenjdk-bin}/bin/java -jar ${modifiedJetty}/start.jar --add-to-start=http,deploy,websocket,ext,jsp,jstl,resources,server
